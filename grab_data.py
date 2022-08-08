b945b12deb969560e44e4e820dd578f016aed52f
aronnyberg <53857832+aronnyberg@users.noreply.github.com>
2022-08-08 16:25:11 +0100

    Create grab_data.py

---
+++ grab_data.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import ccxt
+import calendar
+from datetime import datetime, date, timedelta
+
+binance = ccxt.binance()
+
+def min_ohlcv(dt, pair, limit):
+    # UTC native object
+    since = calendar.timegm(dt.utctimetuple())*1000
+    ohlcv1 = binance.fetch_ohlcv(symbol=pair, timeframe='1m', since=since, limit=limit)
+    ohlcv2 = binance.fetch_ohlcv(symbol=pair, timeframe='1m', since=since, limit=limit)
+    ohlcv = ohlcv1 + ohlcv2
+    return ohlcv
+
+def ohlcv(dt, pair, period='1d'):
+    ohlcv = []
+    limit = 1000
+    if period == '1m':
+        limit = 720
+    elif period == '1d':
+        limit = 365
+    elif period == '1h':
+        limit = 24
+    elif period == '5m':
+        limit = 288
+    for i in dt:
+        start_dt = datetime.strptime(i, "%Y%m%d")
+        since = calendar.timegm(start_dt.utctimetuple())*1000
+        if period == '1m':
+            ohlcv.extend(min_ohlcv(start_dt, pair, limit))
+        else:
+            ohlcv.extend(binance.fetch_ohlcv(symbol=pair, timeframe=period, since=since, limit=limit))
+    df = pd.DataFrame(ohlcv, columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume'])
+    df['Time'] = [datetime.fromtimestamp(float(time)/1000) for time in df['Time']]
+    df['Open'] = df['Open'].astype(np.float64)
+    df['High'] = df['High'].astype(np.float64)
+    df['Low'] = df['Low'].astype(np.float64)
+    df['Close'] = df['Close'].astype(np.float64)
+    df['Volume'] = df['Volume'].astype(np.float64)
+    df.set_index('Time', inplace=True)
+    return df
